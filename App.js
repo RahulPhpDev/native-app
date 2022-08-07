7e7976b3dad613b5b97f2095d714a465a4d67bbb
Rahul <mrrahul2016@gmail.com>
2022-08-07 20:50:43 +0530

    input: update state by input

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View,Image, Button } from 'react-native';
 
 export default function App() {
+  const [inputName, setInputName ] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text> Jai shree Ram</Text>
+      <LoadImage />
+      <StatusBar style="auto"/>
+      <TextInput onChangeText={(newText) => setInputName(newText)} placeholder ="Place your name" style ={
+        {
+          borderWidth: 2,
+          borderColor:'blue',
+          width: 224,
+          height: 40,
+          padding: 5
+        } 
+      }/>
+      <Text > {`name is : ${inputName}`}  </Text>
+      <Button title="Update My Name"  />
     </View>
   );
 }
-
+const LoadImage = (props) => {
+  return (
+    <Image 
+    source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+    // source ={'/logo.png'} 
+    style={{width: 100, height: 60,marginBottom:50}}
+    />
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin:50,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'top',
   },
 });
